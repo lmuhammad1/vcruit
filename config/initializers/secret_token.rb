24b0dca87550f7eba90a90bff5bb61511269e05e
Louis Muhammad <louis.muhammad@gmail.com>
2013-06-26 11:39:35 -0400

    Created vcruit rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vcruit::Application.config.secret_token = 'ac6e1808401a1ee36db050dc22ad176fb2e0f9de6497e49efb21c62ba7cf32d62548dda04a97093add5876cdb465482752f77e5222419131d92c3c9ed1614842'
