21e3db47d25de8860a2be6260dbb35a29f2fa53d
Louis Muhammad <louis.muhammad@gmail.com>
2013-06-27 04:35:35 -0400

    added about and contact pages and edited routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Vcruit::Application.routes.draw do
   resources :static_pages
+  
+  match '/about',  to: 'static_pages#about'
+  match '/contact',  to: 'static_pages#contact'
 
 
   root :to => 'static_pages#index'
