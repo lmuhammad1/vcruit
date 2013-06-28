9589ff854ce510922793fc0531a7270e19d778b9
Louis Muhammad <louis.muhammad@gmail.com>
2013-06-28 02:33:23 -0400

    rails g migration add_auth_token_to_users auth_token:string created remember me cookie

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130627093538) do
+ActiveRecord::Schema.define(:version => 20130628033929) do
 
   create_table "static_pages", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20130627093538) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "auth_token"
   end
 
 end
